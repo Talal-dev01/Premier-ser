d41a6ed2e884fa84b0ae4438bbc547d3dc357022
Talal-dev01 <muhammadtalal@punch.agency>
2025-02-12 20:08:09 +0500

    Fixed Errors

--- Server/config.js
+++ Server/config.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 const config = async () => {
+  console.log('Attempting to connect to MongoDB...');
+    console.log('MONGO_URI:', process.env.MONGO_URI); 
   await mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
