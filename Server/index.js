41e3cf3cb8b42e2b647b63456e2271cc1a170e92
Talal-dev01 <muhammadtalal@punch.agency>
2025-02-12 18:04:15 +0500

    Index fixed

--- Server/index.js
+++ Server/index.js
@@ -232,12 +232,12 @@ app.delete("/api/delete-saved-item", async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
-}
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
